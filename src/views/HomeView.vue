6e5e1a4eca7edd0aa322c0c8e5e7452ff65d751d
jeffspatafora <jeffspatafora@gmail.com>
2022-04-07 12:45:16 -0700

    adds index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,44 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      fruits: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    console.log("in create");
+    this.indexFruits();
+  },
+  methods: {
+    indexFruits: function () {
+      console.log("index fruits");
+      axios.get("/fruits.json").then(response => {
+        console.log(response.data);
+        console.log(response)
+        this.fruits = response.data;
+      });
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for:="fruit in fruits" v-bind:key="fruit.id">
+      <p>{{ fruit.name }}</p>
+      <img v-bind:src="fruit.image" v-bind:alt="fruit.name" />
+      <p>width: {{ fruit.width }}</p>
+    </div>
+    <button v-on:click="indexFruits()">index fruits</button>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 150px;
+}
+</style>
