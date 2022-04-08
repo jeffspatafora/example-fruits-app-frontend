62108a0cdb738fca7fa8a9881c77a7d0b0af428e
jeffspatafora <jeffspatafora@gmail.com>
2022-04-08 00:58:30 -0700

    adds create action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,13 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      fruits: []
+      fruits: [],
+      newFruitParams: {
+        kind: "",
+        name: "",
+        quantity: "",
+        image: ""
+      },
     };
   },
   created: function () {
@@ -17,8 +23,16 @@ export default {
       console.log("index fruits");
       axios.get("/fruits.json").then(response => {
         console.log(response.data);
-        console.log(response)
+        console.log(response);
         this.fruits = response.data;
+      });
+    },
+    createFruit: function () {
+      console.log("create fruits");
+      axios.post("/fruits.json", this.newFruitParams).then(response => {
+        console.log(response.data);
+        this.fruits.push(response.data);
+        this.newFruitsParams = {};
       });
     }
   },
@@ -28,11 +42,29 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>
+      kind
+      <input type="text" v-model="newFruitParams.kind" />
+    </p>
+    <p>
+      name
+      <input type="text" v-model="newFruitParams.name" />
+    </p>
+    <p>
+      quantity
+      <input type="text" v-model="newFruitParams.quantity" />
+    </p>
+    <p>
+      image
+      <input type="text" v-model="newFruitParams.image" />
+    </p>
+    <button v-on:click="createFruit()">add a fruit</button>
     <div v-for:="fruit in fruits" v-bind:key="fruit.id">
       <h3>{{ fruit.name }}</h3>
       <img v-bind:src="fruit.image" v-bind:alt="fruit.name" />
       <p>kind: {{ fruit.kind }}</p>
       <p>quantity: {{ fruit.quantity }}</p>
+      <hr />
     </div>
     <button v-on:click="indexFruits()">index fruits</button>
   </div>
